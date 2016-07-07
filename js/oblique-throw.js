d0736c05a62b755962092c73a4145f859b9608d5
Helivelton Barbosa <helivelton@ic.ufal.br>
2016-07-06 21:20:55 -0300

    calculus and drawing on graph finalized

--- js/oblique-throw.js
+++ js/oblique-throw.js
@@ -1,47 +1,46 @@
 /**
  * Created by mochileiro on 06/07/16.
  */
-$(document).ready(function () {
+$("#singlebutton").click(function () {
 
-    $("#vel-inicial").val(600);
-    $("#angulo").val(1.1);
-    $("#coeficiente").val(0.2);
+    //$("#vel-inicial").val(600);
+    //$("#angulo").val(1.1);
+    //$("#coeficiente").val(0.2);
 
     var tempo = 0.0;
     //document.writeln(tempo);
 
-    var velinicialx = $("#vel-inicial").val() * Math.cos($("#angulo").val());
-    var velinicialy = $("#vel-inicial").val() * Math.sin($("#angulo").val());
+    var graus = $("#angulo").val();
+    var rads = (graus * Math.PI) / 180;
+
+
+    var velinicialx = $("#vel-inicial").val() * Math.cos(rads);
+    var velinicialy = $("#vel-inicial").val() * Math.sin(rads);
 
     //document.writeln("SPEED" + velinicialx);
 
-    var graus = 0;
-    var rads = (graus * Math.PI) / 180;
+
     var gravidade = 9.807;
 
     var posicaox = 0.0;
     var posicaoy = 0.1;
 
-    alert("TESTE2!!!!");
 
 
     var ctx = $("#myCanvas").get(0).getContext('2d');
 
 
-    ctx.fillStyle = "#000000"
+    ctx.fillStyle = "#000000";
     //ctx.fillStyle = "rgb(200,0,0)";
 
 
     //canvas.fillRect(0,0,1,1);
 
 
-    alert("TESTE3!!!!");
-    var cord = 10;
-
     while(posicaoy > 0) {
-        tempo += 0.25;
+        tempo += 0.001;
 
-        cord++;
+
         //document.writeln(tempo);
 
         var repeat = (1 - Math.pow(Math.E, -($("#coeficiente").val() * tempo )));
@@ -50,12 +49,22 @@ $(document).ready(function () {
 
         posicaoy = ( (-(gravidade * tempo)) / $("#coeficiente").val()) + (($("#coeficiente").val() * velinicialy + gravidade) / ($("#coeficiente").val() * $("#coeficiente").val())) * repeat;
 
-        ctx.fillRect( Math.round(posicaox),Math.round(posicaoy),1,1);
-        alert("OK");
+        if ($("#vel-inicial").val() > 100 )
+        {
+            posicaox = posicaox/30;
+            posicaoy = posicaoy/30;
+        }
+        ctx.fillRect( Math.round(posicaox),(300  - Math.round(posicaoy)),1,1);
+
 
     }
 
 
 
-
 });
+
+function () {
+
+
+
+}
